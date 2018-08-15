985e95099f1568a013358bf19061619439d2018d
fernandezgonzalo <fernandezgonzalosebastian@gmail.com>
2018-08-15 09:19:31 -0300

    refactor class main to a single function

--- listener/src/main.py
+++ listener/src/main.py
@@ -1,14 +1,14 @@
-
 from event_buffer import EventBuffer
 from listener import Listener
 from processor import Processor
 
-class Main:
-  def run(self):
+
+def run():
     buffer = EventBuffer()
     processor = Processor(buffer)
     listener = Listener(buffer)
     listener.run()
 
+
 if __name__ == '__main__':
-  Main().run()
+    run()
