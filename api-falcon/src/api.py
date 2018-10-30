5112304f4cf38e71bfec3d40ff3b8ae2bc2b4def
fernandezgonzalo <fernandezgonzalosebastian@gmail.com>
2018-10-30 12:45:50 -0300

    add api debt

--- api-falcon/src/api.py
+++ api-falcon/src/api.py
@@ -1,7 +1,7 @@
 import falcon
 
-from resources import LoanItem
-from resources import LoanList
+from resources import DebtItem
+from resources import DebtList
 from resources import HealthStatusResource
 from falcon_cors import CORS
 import db
@@ -10,6 +10,8 @@ cors = CORS(allow_all_origins=True)
 
 api = application = falcon.API(middleware=[cors.middleware])
 
-api.add_route("/v1/loans/", LoanList())
-api.add_route("/v1/loans/{loan_index}/", LoanItem())
+# api.add_route("/v1/loans/", LoanList())
+# api.add_route("/v1/loans/{loan_index}/", LoanItem())
 api.add_route("/health_status/", HealthStatusResource())
+api.add_route("/v4/debts/", DebtList())
+api.add_route("/v4/debts/{id_loan}/", DebtItem())
