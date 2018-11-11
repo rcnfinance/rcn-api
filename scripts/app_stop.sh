f62d69695d0efc88bc394fb7ad02e83c94c3877c
Agustin Aguilar <agustin@ripiocredit.network>
2018-11-11 01:38:33 +0100

    Fix replace files and app stop

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Stoping container"
-[ "$(docker ps -q)" ] && docker stop $(docker ps -aq)
+[ "$(docker ps -q)" ] && docker stop $(docker ps -aq) || true
 echo "Removing container"
-[ "$(docker ps -aq)" ] && docker rm -f $(docker ps -aq)
+[ "$(docker ps -aq)" ] && docker rm -f $(docker ps -aq) || true
 exit 0
