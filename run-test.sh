7a761222ac499c19b0b08bd96091ff88912a1932
Fernandez Gonzalo <fernandezgonzalosebastia@gmail.com>
2019-03-18 10:41:05 -0300

    wip

---
+++ run-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dockerc=docker-compose
+$dockerc -f docker-compose-test.yml down && $dockerc -f docker-compose-test.yml up -d mongo api-falcon listener ganachecli && $dockerc -f docker-compose-test.yml up test-truffle
