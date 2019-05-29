872eda11d714255557d912c2484fef9b219ef23e
Fernandez Gonzalo <fernandezgonzalosebastia@gmail.com>
2019-05-29 03:27:09 -0300

    remove unused _data value

--- listener/src/contracts/debtEngine/handlers/created.py
+++ listener/src/contracts/debtEngine/handlers/created.py
@@ -9,12 +9,6 @@ class Created(EventHandler):
     signature = "Created(bytes32,uint256,bytes)"
     signature_hash = web3.Web3.sha3(text=signature).hex()
 
-    # def _parse(self):
-    #     self._data = self._event.get("data")
-    #     self._id = self._event.get("topics")[1].hex()
-    #     self._block_number = self._event.get('blockNumber')
-    #     self._transaction = self._event.get('transactionHash').hex()
-
     def _normalize(self):
         self._args["_id"] = utils.add_0x_prefix(self._args["_id"].hex())
 
@@ -37,7 +31,6 @@ class Created(EventHandler):
         created = str(self._block_timestamp())
 
         data = {
-            "_data": self._args.get("data"),
             "error": error,
             "balance": str(balance),
             "model": model,
