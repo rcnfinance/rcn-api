a611f9150dc98398ae741546b6cc7e9577bf9d4a
Fernandez Gonzalo <fernandezgonzalosebastia@gmail.com>
2019-06-09 07:47:04 -0300

    commit2collection

--- listener/src/handlers/approval_for_all_handler.py
+++ listener/src/handlers/approval_for_all_handler.py
@@ -2,6 +2,7 @@ import web3
 from .event_handler import EventHandler
 from handlers import utils
 
+
 class ApprovalForAllHandler(EventHandler):
     signature = 'ApprovalForAll(address,address,bool)'
     signature_hash = web3.Web3.sha3(text=signature)
