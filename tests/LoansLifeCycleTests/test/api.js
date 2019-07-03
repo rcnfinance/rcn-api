a7a3c571c64aa94075046989e582d59932192e44
rotcivegaf <victorfage@gmail.com>
2019-07-03 16:37:13 -0300

    refactor api.js

--- tests/LoansLifeCycleTests/test/api.js
+++ tests/LoansLifeCycleTests/test/api.js
@@ -1,54 +1,32 @@
 const axios = require('axios');
-const base_url = process.env.BASE_URL;
+const baseUrl = process.env.BASE_URL;
 
-async function get_loan (id_loan) {
-  const endpoint_resource = 'loans/';
-  const url = base_url + endpoint_resource + id_loan;
-
-  response = await axios.get(url);
-  return response.data;
+module.exports.getLoan = async (idLoan) => {
+  const endpointResource = 'loans/';
+  return this.get(endpointResource + idLoan);
 };
 
-async function get_debt (id_debt) {
-  const endpoint_resource = 'debts/';
-  const url = base_url + endpoint_resource + id_debt;
-
-  response = await axios.get(url);
-
-  return response.data;
+module.exports.getDebt = async (idDebt) => {
+  const endpointResource = 'debts/';
+  return this.get(endpointResource + idDebt);
 };
 
-async function get_config (id_config) {
-  const endpoint_resource = 'configs/';
-  const url = base_url + endpoint_resource + id_config;
-
-  response = await axios.get(url);
-
-  return response.data;
+module.exports.getConfig = async (idLoan) => {
+  const endpointResource = 'configs/';
+  return this.get(endpointResource + idLoan);
 };
 
-async function get_state (id_state) {
-  const endpoint_resource = 'states/';
-  const url = base_url + endpoint_resource + id_state;
-
-  response = await axios.get(url);
-
-  return response.data;
+module.exports.getState = async (idLoan) => {
+  const endpointResource = 'states/';
+  return this.get(endpointResource + idLoan);
 };
 
-async function get_model_debt_info (id_loan) {
-  const endpoint_resource = 'model_debt_info/';
-  const url = base_url + endpoint_resource + id_loan;
-
-  response = await axios.get(url);
-
-  return response.data;
+module.exports.getModelDebtInfo = async (idLoan) => {
+  const endpointResource = 'model_debt_info/';
+  return this.get(endpointResource + idLoan);
 };
 
-module.exports = {
-  get_loan: get_loan,
-  get_debt: get_debt,
-  get_config: get_config,
-  get_state: get_state,
-  get_model_debt_info: get_model_debt_info,
+module.exports.get = async (endpointResource) => {
+  const response = await axios.get(baseUrl + endpointResource);
+  return response.data;
 };
