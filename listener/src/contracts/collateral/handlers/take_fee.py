506547250e86c0b833fc715522611eb3b4e14920
NicolasMenendez <menendeznm@gmail.com>
2019-07-24 15:35:35 -0300

    create events handlers for collateral contract

---
+++ listener/src/contracts/collateral/handlers/take_fee.py
@@ -0,0 +1,28 @@
+import web3
+from contracts.event import EventHandler
+from models import Commit
+# import utils
+
+
+class TakeFee(EventHandler):
+    signature = "TakeFee(parameters..)"
+    signature_hash = web3.Web3.sha3(text=signature).hex()
+
+    # def _normalize(self):
+    #     self._args["_id"] = utils.add_0x_prefix(self._args["_id"].hex())
+
+    def handle(self):
+        commit = Commit()
+
+        commit.opcode = "take_fee_collateral"
+        commit.timestamp = self._block_timestamp()
+        commit.proof = self._transaction
+
+        data = {
+            # "id": self._args.get("_id"),
+            # "approved": True
+        }
+
+        commit.data = data
+
+        return [commit]
