f45be6c8bbf7a5367158da4fe4de0ec180a93c35
Nicolas <menendeznm@gmail.com>
2019-08-08 14:52:45 -0300

    Add discord webhook for push notifications

--- listener/src/contracts/oracleFactory/commit_processors/provide.py
+++ listener/src/contracts/oracleFactory/commit_processors/provide.py
@@ -1,6 +1,6 @@
 from models import OracleRate
 from contracts.commit_processor import CommitProcessor
-
+import requests
 
 class Provide(CommitProcessor):
     def __init__(self):
@@ -15,7 +15,27 @@ class Provide(CommitProcessor):
         oracleRate.signer = data.get("signer")
         oracleRate.rate = data.get("rate")
 
-        print(commit)
-       
+        # defining the api-endpoint  
+        API_ENDPOINT = "https://discordapp.com/api/webhooks/609025990974505000/FiC9Q5JpAfTmb35kCeHhHqwb7fCcCtg9bPv5eq73XDNPHSsPaL1ymhV4kzAcTCgRsHFy"
+
+        separation = '-----' + '\n' 
+        title = 'New Rate Provided:' + '\n' 
+        oracle = 'Oracle: ' + oracleRate.oracle + '\n'
+        signer = 'Signer:' + oracleRate.signer + '\n'
+        rate = 'Rate:' + oracleRate.rate + '\n'  
+
+        rateProvided = separation + title + oracle + signer + rate + separation         
+
+        # data to be sent to api 
+        payload = {'username':'Test', 
+                    'content': rateProvided } 
+        
+        # sending post request and saving response as response object 
+        r = requests.post(url = API_ENDPOINT, data = payload) 
+        
+        # extracting response text  
+        oracle_url = r.text 
+        print("The oracle URL is:%s"%oracle_url) 
+
         commit.save()
         oracleRate.save()
